119aa62038fb390f94a1f66ec94b34fe8843a7f6
Ignacio Gonzalez <nachogonzalez2727@gmail.com>
2025-05-28 11:26:03 -0300

    feat: added invoice page and metadata to all pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Customers",
+    description: "Customers section"
+}
+
 const Customers = () => {
     return (
         <p>Customers Page</p>
